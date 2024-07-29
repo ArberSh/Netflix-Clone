b6bd8197f69782160caf11d6df9a2fbed142fc69
Arber Shaska <arbershaska2@gmail.com>
2024-07-29 23:37:01 +0200

    Added Sign In

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Introduction from '../src/Introduction'
+
+function App() {
+
+  return (
+    <>
+    <Introduction/>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    
+    colors:{
+      'red':'#db0000',
+      'black':'#000000',
+      'white':'#ffffff',
+      'gray':'#564d4d',
+      'dark-red':'#831010'
+    },
+    extend: {
+      backgroundImage:{
+        'Background-Netflix': "linear-gradient(to right bottom, rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.1)),url('/src/assets/Netflix-Background.jpg')"
+      },
+    },
+  },
+  plugins: [],
+}
+
