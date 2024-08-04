cf5f7682f06c3760618798be43d4b68446853cb2
Arber Shaska <arbershaska2@gmail.com>
2024-08-04 22:55:25 +0200

    Sign Up Finished

--- my-project/src/Introduction.tsx
+++ my-project/src/Introduction.tsx
@@ -20,8 +20,9 @@ function Introduction() {
     
     const [clicked, SetClicked] = useState<Boolean>(false);
     const [SignInclicked, SetSignInClicked] = useState<Boolean>(false);
+    const [ErrorMessage,setErrorMessage] = useState<boolean>(false)
 
-    const { register,handleSubmit,formState:{errors},watch,setValue,getValues } = useForm<FormFields>();
+    const { register,handleSubmit,formState:{errors},watch,setValue } = useForm<FormFields>();
     const navigate = useNavigate();
     
     const onSubmit: SubmitHandler<FormFields> = (data) => {
@@ -38,8 +39,14 @@ function Introduction() {
         navigate('/HomePage')
         SetLoading(false)
     }).catch((error)=>{
+        SetLoading(false)
         const errorCode = error.code
         const errorMessage = error.message
+        if (errorCode === 'auth/email-already-in-use') {
+        setErrorMessage(true);
+      } else {
+        setErrorMessage(false);
+      }
         console.error(errorCode, errorMessage);
     })
   }
@@ -62,7 +69,8 @@ function Introduction() {
       </div>
       {clicked ? (
         <div className="flex flex-col justify-center  items-center">
-          <form onSubmit={handleSubmit(onSubmit)} className=" p-10 bg-black flex flex-col items-start">
+          <div  className=" p-10 bg-black flex flex-col items-start">
+            <form onSubmit={handleSubmit(onSubmit)} className="  bg-black flex flex-col items-start">
             <h1 className="text-white text-3xl font-bold">Sign Up</h1>
             <div className={` w-80 gap-4 flex mt-4 ${errors.Firstname || errors.Lastname ? "mb-2" : "mb-4"}`}>
                  
@@ -89,7 +97,7 @@ function Introduction() {
                 {errors.Firstname ? (<div className="text-red mb-2 font-bold">{errors.Firstname.message}</div>):
                 (errors.Lastname && <div className="text-red mb-2 font-bold">{errors.Lastname.message}</div>)}
             <input
-              className={`px-2 rounded h-12 w-full outline-none ${errors.Email ? ("mb-2"):("mb-4")} `}
+              className={`px-2 rounded h-12 w-full outline-none ${(errors.Email || ErrorMessage) ? ("mb-2"):("mb-4")} `}
               type="text"
               id=""
               placeholder="Email"        
@@ -101,6 +109,7 @@ function Introduction() {
                   }
               })}
             />
+              {ErrorMessage && <div className="text-red mb-2 font-bold">Email is already used</div>}
               {errors.Email && <div className="text-red mb-2 font-bold">{errors.Email.message}</div>}
             <input
               className={`px-2 rounded h-12  w-full outline-none ${errors.Password ? ("mb-2"):("mb-4")}`}
@@ -134,11 +143,13 @@ function Introduction() {
             <button type="submit" className=" flex items-center justify-center my-2 w-full bg-red px-6 py-2 text-white text-lg font-bold">
               {Loading ? (<img className="w-7 animate-spin text-center" src={LoadingImg}></img>) : ("Sign Up")}
             </button>
+            </form>
+           
             <div className="flex gap-6 mt-2">
               <p className="text-gray font-bold">Already Have an account?</p>
-              <button className="text-white font-bold">Sign In Now</button>
+              <button onClick={SignInClicked} className="text-white font-bold">Sign In Now</button>
             </div>
-          </form>
+           </div>
         </div>
       ) : (
         <div className="px-6 flex justify-center items-center flex-col h-3/4 text-center">
